1ac7ee7a499e8884600fa467ef3ea642bc496f36
Enzozo ANDRE <enzoandre218@gmail.com>
2023-03-29 16:50:17 +0200

    exercice props 2

---
+++ exercice pokemon/src/App.jsx
@@ -0,0 +1,21 @@
+import PokemonCards from "./components/PokemonCard";
+
+function App() {
+  return (
+    <div>
+      <PokemonCards name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
+    </div>
+  )
+}
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+export default App
---
+++ exercice pokemon/src/components/PokemonCard.jsx
@@ -0,0 +1,28 @@
+function PokemonCards(props) {
+    const pokemon = props
+
+    return (
+        <figure>
+            {(pokemon.imgSrc != null) ? (
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+                    alt="bulbasaur" />) : (
+                <p>???</p>
+            )}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+
+    );
+}
+
+PokemonCards.prototype = {
+    props: PropTypes.shape({
+        name: PropTypes.string.isrequired,
+        imgSrc: PropTypes.string,
+    }).isRequired,
+}
+
+
+
+
+
+export default PokemonCards;
