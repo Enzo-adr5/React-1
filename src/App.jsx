70cf25a9d291a5003c91f88e93472c96f1a1cf68
Enzo <127224391+Enzo-adr5@users.noreply.github.com>
2023-04-17 17:00:33 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import PokemonCards from "./components/PokemonCard";
-
-function App() {
-  return (
-    <div>
-      <PokemonCards name={pokemonList[0].name} imgSrc={pokemonList[0].imgSrc} />
-    </div>
-  )
-}
+import { useState } from "react";
+import "./App.css";
+import PokemonCard from "./components/PokemonCard";
 const pokemonList = [
   {
     name: "bulbasaur",
@@ -14,8 +8,46 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
-export default App
+function App() {
+  const [index, setIndex] = useState(0);
+  let pokemon = pokemonList[index];
+  const previousPokemon = () => {
+    if (index > 0) {
+      setIndex(index - 1);
+    }
+  };
+  const nextPokemon = () => {
+    if (index < pokemonList.length - 1) {
+      setIndex(index + 1);
+    }
+  };
+
+  return (
+    <div className="App">
+      <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc} />
+      <button onClick={previousPokemon}>Précédent</button>
+      <button onClick={nextPokemon}>Suivant</button>
+    </div>
+  );
+}
+
+export default App;
